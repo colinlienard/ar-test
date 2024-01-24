c9de7b5af18a3f2711b3814350ac13bbb73b2789
Colin Lienard <colin.lienard87@gmail.com>
2024-01-24 21:02:27 +0100

    feat: lighting based on environment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import {
-  BoxGeometry,
+  DataTexture,
   HemisphereLight,
   Mesh,
   MeshBasicMaterial,
+  MeshStandardMaterial,
   PerspectiveCamera,
   RingGeometry,
   Scene,
+  SphereGeometry,
   WebGLRenderer,
 } from "three";
-import { ARButton } from "three/examples/jsm/Addons.js";
+import { ARButton, XREstimatedLight } from "three/examples/jsm/Addons.js";
 
+// Setup
 const { devicePixelRatio, innerHeight, innerWidth } = window;
 const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(innerWidth, innerHeight);
@@ -18,7 +21,9 @@ renderer.xr.enabled = true;
 
 document.body.appendChild(renderer.domElement);
 document.body.appendChild(
-  ARButton.createButton(renderer, { requiredFeatures: ["hit-test"] })
+  ARButton.createButton(renderer, {
+    requiredFeatures: ["hit-test", "light-estimation"],
+  })
 );
 
 const scene = new Scene();
@@ -29,16 +34,37 @@ const camera = new PerspectiveCamera(
   20
 );
 
-const boxGeometry = new BoxGeometry(0.1, 0.1, 0.1);
-const boxMaterial = new MeshBasicMaterial({ color: 0xff0000 });
+// Light
+let defaultEnvironment: DataTexture;
+const defaultLight = new HemisphereLight(0xffffff, 0xbbbbff, 1);
+defaultLight.position.set(0.5, 1, 0.25);
+scene.add(defaultLight);
+const xrLight = new XREstimatedLight(renderer);
+xrLight.addEventListener("estimationstart", () => {
+  scene.add(xrLight);
+  scene.remove(defaultLight);
+  if (xrLight.environment) {
+    scene.environment = xrLight.environment;
+  }
+});
+xrLight.addEventListener("estimationend", () => {
+  scene.add(defaultLight);
+  scene.remove(xrLight);
+  scene.environment = defaultEnvironment;
+});
+
+// Sphere
+const boxGeometry = new SphereGeometry(0.1, 32, 32);
+const boxMaterial = new MeshStandardMaterial({
+  color: 0xffd700,
+  roughness: 0.5,
+  metalness: 0.5,
+});
 const box = new Mesh(boxGeometry, boxMaterial);
 box.position.z = -1;
 scene.add(box);
 
-const light = new HemisphereLight(0xffffff, 0xbbbbff, 3);
-light.position.set(0.5, 1, 0.25);
-scene.add(light);
-
+// Reticle
 const reticle = new Mesh(
   new RingGeometry(0.15, 0.2, 32).rotateX(-Math.PI / 2),
   new MeshBasicMaterial()
